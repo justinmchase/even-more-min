68d69ef1ddff9df1b40587e4b7f3f87785b6972e
Justin Chase <jchase@structural.com>
2019-01-02 10:01:07 -0600

    fix inherits reference

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-var mocha = require('mocha')
-var inherits = require('../utils').inherits;
+const mocha = require('mocha')
+const inherits = require('util').inherits
+const Base = mocha.reporters.Base
 
 function EvenMoreMin(runner) {
-  Base.call(this, runner);
-  runner.once('end', this.epilogue.bind(this));
+  Base.call(this, runner)
+  runner.once('end', this.epilogue.bind(this))
 }
 
-inherits(EvenMoreMin, Base);
-exports = module.exports = EvenMoreMin;
+inherits(EvenMoreMin, Base)
+exports = module.exports = EvenMoreMin
